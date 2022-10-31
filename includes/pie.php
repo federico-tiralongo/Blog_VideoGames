cd0cc5b54a36d487bba2bf0978d0b614cf38d7df
federicotiralongo95@gmail.com <federicotiralongo95@gmail.com>
2022-10-31 15:44:07 +0100

    mi proyecto

---
+++ includes/pie.php
@@ -0,0 +1,11 @@
+
+			<div class="clearfix"></div>
+		</div> <!-- fin contenedor -->
+
+		<!-- PIE DE PÁGINA -->
+		<footer id="pie">
+			<p>Proyecto de Federico Tiralongo &copy; 2022</p>
+		</footer>
+		
+	</body>
+</html>
